4d698c8053ddbe87d80c4a52056a89c6607efab6
maxkub <maxime.kubryk@gmail.com>
2014-11-11 18:31:04 +0100

    test

--- neural_training_test/Source.cpp
+++ neural_training_test/Source.cpp
@@ -1,7 +1,7 @@
 #include "F:/Projets-C++/neural_network/neural_network/Neural_Network.h"
 #include "F:/Projets-C++/neural_network/neural_network/Back_prop.h"
 
-int main
+int main()
 {
 
 	return 0;
