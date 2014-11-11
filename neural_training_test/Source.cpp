c54828d6c68278521dcfd4ecc55fa025114f79e2
maxkub <maxime.kubryk@gmail.com>
2014-11-11 23:38:44 +0100

    1

--- neural_training_test/Source.cpp
+++ neural_training_test/Source.cpp
@@ -1,9 +1,48 @@
+#include "stdafx.h"
 #include "F:/Projets-C++/neural_network/neural_network/Neural_Network.h"
 #include "F:/Projets-C++/neural_network/neural_network/Back_prop.h"
+//#include "F:/Projets-C++/neural_network/neural_network/Neuron.h"
+//#include "F:/Projets-C++/neural_network/neural_network/Layer.h"
+#include <vector>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
 
+	vector<int> scheme = { 1, 5, 1 };
+
+	string path = "F:/Projets-C++/neural_training_test/sinus_training.csv";
+	vector<double> training_inputs;
+	vector<double> training_outputs;
+
+	fstream file(path.c_str());
+
+	if (file)
+	{
+		double x, y;
+		char separator;
+
+		while (file >> x)
+		{
+			file >> separator;
+			file >> y;
+			file.ignore(1000, '\n');
+
+			training_inputs.push_back(x);
+			training_outputs.push_back(y);
+		} 
+	}
+	else
+	{
+		cout << "ERROR in opening file : " << path.c_str() << endl;
+	}
+
+	Network network(scheme, 1);
+
+	network.build_network();
 
 
 
